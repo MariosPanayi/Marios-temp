435a2672f028960556b76fdb31a5cda0ed422f12
MariosPanayi <mariooosp@gmail.com>
2018-03-18 21:38:02 +0000

    Test params

---
+++ GLRA002_Subject_Info.m
@@ -0,0 +1,62 @@
+
+name = {'GLRA50.6d'
+'GLRA52.4d'
+'GLRA53.5f'
+'GLRA64.1c'
+'GLRA64.1e'
+'GLRA54.3d'
+'GLRA56.2a'
+'GLRA65.1a'
+'GLRA62.4b'
+'GLRA58.3d'
+'GLRA58.3b'
+'GLRA65.2a'
+'GLRA58.3c'};
+
+sex = {'Female'
+'Female'
+'Female'
+'Female'
+'Female'
+'Female'
+'Male'
+'Male'
+'Male'
+'Male'
+'Male'
+'Male'
+'Male'};
+
+Genotype = {'WT'
+'KO'
+'KO'
+'WT'
+'WT'
+'KO'
+'WT'
+'WT'
+'KO'
+'KO'
+'WT'
+'KO'
+'WT'};
+
+%nm/nA
+calibrationfactor = [0.04072445
+0.086679
+0.08166935
+0.025954
+0.0297486
+0.1057612
+0.0167
+0.0105
+0.02465
+0.0089
+0.0194
+0.016
+0.01435];
+        %% Plot
+      close all
+        i = 12;
+        h = plot_fcv_trials_Anaesthetized(data(i).processed.model_cvs, data(i).processed.ts, data(i).raw.TTLs, params, data(i).processed.c_predicted);
+         %suptitle([params.fig_title]);
