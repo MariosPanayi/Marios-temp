2a8205872abdd471ef2b56c6dc91fdbfaa72afb6
MariosPanayi <mariooosp@gmail.com>
2018-07-16 20:28:45 +0100

    update

--- MP009_Layla/AAB_DataTableMarios.m
+++ MP009_Layla/AAB_DataTableMarios.m
@@ -24,9 +24,41 @@ databins.sem = databins.std/(sqrt(databins.size(1)));
 %sem = @(std,n) (std/sqrt(n-1))
 
 
-%pull out first min
-firstminbin = databins(:, {'Genotype','Animal', 'Stage', 'minbins'});
-[G2,firstminbin] = findgroups(firstminbin.minbins == 1);
-firstminbin.distance = splitapply(databins.distance,G2) 
+%pull out first min into new table
+firstminbin = databins(databins.minbins ==1, :);
 
-[a,b] = findgroups(databins.minbins == 1)
+%group averages of first min
+grp_avg_1stmin = firstminbin(:, {'Genotype', 'Stage'});
+[G3,grp_avg_1stmin] = findgroups(grp_avg_1stmin);
+
+grp_avg_1stmin.avgDistance = splitapply(@mean, firstminbin.sumDistance, G3);
+grp_avg_1stmin.std = splitapply(@std, firstminbin.sumDistance, G3);
+grp_avg_1stmin.size = splitapply(@size, firstminbin.sumDistance, G3);
+grp_avg_1stmin.sem = grp_avg_1stmin.std/(sqrt(grp_avg_1stmin.size(1)));
+grp_avg_1stmin.lowerERR = grp_avg_1stmin.avgDistance - grp_avg_1stmin.sem;
+grp_avg_1stmin.upperERR = grp_avg_1stmin.avgDistance + grp_avg_1stmin.sem;
+
+%%
+%Perform computations on each mouse x trial
+summaryData = T(:, {'Animal', 'Stage', 'Genotype'});
+[G4,summaryData] = findgroups(summaryData);
+
+%To apply complex functions you may need to ouput the data as a cell array
+%then unpack the cellarray before attaching it to the table
+%define smooth function and identify out put as cells {}
+smooth3 = @(x) {smooth(x,18)};
+smoothed = splitapply(smooth3, T.Distance,G4);
+%Unpack the cell array then attach to dataTable
+smoothed = vertcat(smoothed{:});
+T.smooth3 = smoothed;
+
+
+%Saving a table
+writetable(T,'C:\Users\mpanagi\Documents\GitHub\Marios-temp\MP009_Layla\AABsmoothed.xlsx')
+
+
+%summaryData.peak60s = splitapply(@mean, T.Distance, G4);
+
+
+
+
