cbc5f0016300c369dac705b28408fe3c74e8b5c9
MariosPanayi <mariooosp@gmail.com>
2018-08-30 11:45:02 +0100

    update

--- TB1_SandersonData/RM_ANOVA.m
+++ TB1_SandersonData/RM_ANOVA.m
@@ -13,11 +13,11 @@ multcompare(rm,'Same_Diff')
 
 t2.Properties.VariableNames = {'Geno', 'Uid', 'Same1', 'Same2', 'Diff1','Diff2'}
 WithinFactors = table({'same', 'same', 'diff', 'diff'}', {'Bin1' 'Bin2' 'Bin1' 'Bin2'}','VariableNames',{'Same_Diff', 'Bin'});
-rm2 = fitrm(t2, 'Same1-Diff2 ~ Geno', 'WithinDesign',WithinFactors, 'WithinModel', 'Same_Diff+Bin+Same_Diff*Bin');
+[~,WithinFactors.Interaction] = unique(WithinFactors);
+WithinFactors.Interaction = categorical(WithinFactors.Interaction)
+rm2 = fitrm(t2, 'Same1-Diff2 ~ Geno', 'WithinDesign',WithinFactors, 'WithinModel', 'Same_Diff*Bin');
 
-Wsubj = ranova(rm2,'WithinModel', 'Same_Diff+Bin+Same_Diff*Bin')
-Bsubj = anova(rm2)
+Wsubj = ranova(rm2, 'WithinModel', 'Same_Diff*Bin')
 
 multcompare(rm2,'Same_Diff', 'By', 'Bin', 'By','Geno')
-multcompare(rm2,'Geno')
-multcompare(rm2,'Same_Diff')
+multcompare(rm2,'Same_Diff', 'By', 'Bin')
