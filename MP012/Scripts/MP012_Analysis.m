5e097f685a3c8b21c37b450709eb5e5d793c0d5e
MariosPanayi <mariooosp@gmail.com>
2019-10-19 15:30:42 +0100

    Update MP012_Analysis.m

--- MP012/Scripts/MP012_Analysis.m
+++ MP012/Scripts/MP012_Analysis.m
@@ -1,31 +1,67 @@
-%% Extract Raw Data 
+%% Extract Raw Data
 filepath = "C:\Users\Marios\Documents\GitHub\Marios-temp\MP012\RawData\";
 filename = "MP_LPCD_1Lever_Degraded_Day1";
-data = mpc_read_multiple_data(strcat(filepath,filename));
+data_raw = mpc_read_multiple_data(strcat(filepath,filename));
 
-%% List of Event IDs array A = Event, array B = Time
+%% List of relevant Event IDs array A = Event, array B = Time
 % \1 = TrialChange
-% \2 = RewardDelivery
-% \3 = MagazineEntry
-% \4 = MagazineExit
 % \5 = LeftLeverResponse
-% \6 = LeftLeverResponseEnd
 % \7 = RightLeverResponse
-% \8 = RightLeverResponseEnd
 % \9 = LeftNP
-% \10 = LeftNPEnd
 % \11 = RightNP
-% \12 = RightNPEnd
-% \13 = CS1On
-% \14 = CS1Off
-% \15 = CS2On
-% \16 = CS2Off
+% \13 = leftRewardOn
+% \15 = rightRewardOn
 % \17 = ITI start
-% \18 = NonContingent Cue
+% \20 = NonContingent Cue
+% M(1) = ^LeftReward,
+% M(2) = ^RightReward,
+% M(5) = ^DegradedLever,
+% M(6) = ^DegradedRewardID
 
-subj = 1;
-data{1,subj}.A
-data{1,subj}.B
+for subj = 1:size(data_raw,2)
+    %General Parameters
+    data(subj).program = data_raw{1,subj}.MSN;
+    data(subj).mouse = data_raw{1,subj}.Subject;
+    %Rewarded Lever
+    data(subj).leftReward = data_raw{1,subj}.M(1+1);
+    data(subj).rightReward = data_raw{1,subj}.M(2+1);
+    data(subj).noncontingentReward = data_raw{1,subj}.M(6+1);
+    
+    
+    %N.B. only works for 1 lever design and not in extinction
+    if data(subj).leftReward > 0
+        data(subj).rewardedLever = 1;
+        data(subj).contingentReward = data(subj).leftReward;
+        data(subj).contRewardID = 13;
+        %event flags for rewarded and nonrewarded LPs
+        data(subj).LPr = 5;
+        data(subj).LPnr = 7;
+    elseif data(subj).rightReward > 0
+        data(subj).rewardedLever = 2;
+        data(subj).contingentReward = data(subj).rightReward;
+        data(subj).contRewardID = 15;
+        %event flags for rewarded and nonrewarded LPs
+        data(subj).LPr = 7;
+        data(subj).LPnr = 5;
+    end
+    
+    if data(subj).contingentReward == data(subj).noncontingentReward
+        data(subj).condition = "Degraded";
+    else
+        data(subj).condition = "NonDegraded";
+    end
+    
+    data(subj).event = data_raw{1,subj}.A;
+    data(subj).ts = data_raw{1,subj}.B;
+end
 
 
+%%
+
+for subj = 1:size(data,2)
+    data(subj).LPr_total = sum(data(subj).event == data(subj).LPr);
+    data(subj).LPnr_total = sum(data(subj).event == data(subj).LPnr);
+    data(subj).ILPrI = diff(data(subj).ts((data(subj).event == data(subj).LPr)));
+    
+end
 
