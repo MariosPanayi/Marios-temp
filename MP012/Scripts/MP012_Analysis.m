b64f0a61727bedf2bb94eb315b9f0a2d27b6a806
MariosPanayi <mariooosp@gmail.com>
2019-10-22 11:43:30 +0100

    Update MP012_Analysis.m

--- MP012/Scripts/MP012_Analysis.m
+++ MP012/Scripts/MP012_Analysis.m
@@ -1,3 +1,4 @@
+tic
 %% Extract Raw Data
 filepath = "C:\Users\Marios\Documents\GitHub\Marios-temp\MP012\RawData\";
 filename = "MP_LPCD_1Lever_Degraded_Day1";
@@ -26,7 +27,7 @@ for subj = 1:size(data_raw,2)
     data(subj).leftReward = data_raw{1,subj}.M(1+1);
     data(subj).rightReward = data_raw{1,subj}.M(2+1);
     data(subj).noncontingentReward = data_raw{1,subj}.M(6+1);
-    
+    data(subj).noncontRewardID = 20;
     
     %N.B. only works for 1 lever design and not in extinction
     if data(subj).leftReward > 0
@@ -56,19 +57,58 @@ for subj = 1:size(data_raw,2)
 end
 
 
-%%
+%% Summarise Data
 
 for subj = 1:size(data,2)
+    
     %Total LP for the session
     data(subj).LPr_total = sum(data(subj).event == data(subj).LPr);
     data(subj).LPnr_total = sum(data(subj).event == data(subj).LPnr);
+    
     %Inter LP interval on a given lever
     data(subj).ILPrI = diff(data(subj).ts((data(subj).event == data(subj).LPr)));
     data(subj).ILPnrI = diff(data(subj).ts((data(subj).event == data(subj).LPnr)));
+    
     %LP in 5s bins across the session
     sessionLength = 1800;
     binwidth = 5;
-    [N,edges,bin] = histcounts(data(subj).ts((data(subj).event == data(subj).LPr)), [0:binwidth:sessionLength]);
+    [data(subj).fiveSec.N_LPr,data(subj).fiveSec.edges_LPr,data(subj).fiveSec.bin_LPr] = histcounts(data(subj).ts((data(subj).event == data(subj).LPr)),[0:binwidth:sessionLength]);
+    [data(subj).fiveSec.N_LPnr,data(subj).fiveSec.edges_LPnr,data(subj).fiveSec.bin_LPnr] = histcounts(data(subj).ts((data(subj).event == data(subj).LPnr)),[0:binwidth:sessionLength]);
+    [data(subj).fiveSec.N_ContR,data(subj).fiveSec.edges_ContR,data(subj).fiveSec.bin_ContR] = histcounts(data(subj).ts((data(subj).event == data(subj).contRewardID)),[0:binwidth:sessionLength]);
+    [data(subj).fiveSec.N_NContR,data(subj).fiveSec.edges_NContR,data(subj).fiveSec.bin_NContR] = histcounts(data(subj).ts((data(subj).event == data(subj).noncontRewardID)),[0:binwidth:sessionLength]);
+    
+    %LP during contingent cue
+    binwidth = 1;
+    prepostTime = 30;
+    cueDuration = 5;
+    contingentRewardTimes = data(subj).ts(data(subj).event == data(subj).contRewardID);
+    if contingentRewardTimes
+        for i = 1:size(contingentRewardTimes,2)
+            [data(subj).contingentCuePeriod.LPr(i,:)] = histcounts(data(subj).ts((data(subj).event == data(subj).LPr)),[contingentRewardTimes(i)-prepostTime:binwidth:contingentRewardTimes(i)+cueDuration+prepostTime]);
+            [data(subj).contingentCuePeriod.LPnr(i,:)] = histcounts(data(subj).ts((data(subj).event == data(subj).LPnr)),[contingentRewardTimes(i)-prepostTime:binwidth:contingentRewardTimes(i)+cueDuration+prepostTime]);
+            
+        end
+    else
+        data(subj).contingentCuePeriod.LPr = [];
+        data(subj).contingentCuePeriod.LPnr = [];
+    end
+    
+    figure
+plot(mean(data(subj).contingentCuePeriod.LPr))
+title(["Subject "; num2str(subj)]);
+hold on
+plot(mean(data(subj).contingentCuePeriod.LPnr))
+hold off
+
     
 end
+%%
+toc
 
+%%
+% figure
+% plot(mean(data(subj).contingentCuePeriod.LPr))
+% title(["Subject "; num2str(subj)]);
+% hold on
+% plot(mean(data(subj).contingentCuePeriod.LPnr))
+% hold off
