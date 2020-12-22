c9f92cd2df206badc381f6322ad49e5e38d5cdcf
MariosPanayi <mariooosp@gmail.com>
2020-12-21 23:10:26 -0500

    update

---
+++ NIDA_Expts/Experiments/MPNIDA001_Pilots/scripts/Plot_1_SpecificCI.R
@@ -0,0 +1,97 @@
+##### Load relevant packages ----
+## Packages for data organisation and plotting
+library(tidyverse)
+# Package for relative file paths
+library(here)
+# library(ggpubr)
+library(cowplot)
+library(ggsignif)
+library(patchwork)
+library(RColorBrewer)
+## Packages for Data analysis
+library(afex)
+afex_options(emmeans_model = "multivariate")# use multivariate model for all follow-up tests.
+library(emmeans)
+# install.packages("devtools")
+# devtools::install_github("crsh/papaja")
+# library(papaja)
+# library(knitr)
+# remotes::install_github("noamross/redoc")
+# library(redoc)
+
+#### Plot functions ====
+# modified from from:
+# https://stackoverflow.com/questions/39071002/moving-x-or-y-axis-together-with-tick-labels-to-the-middle-of-a-single-ggplot-n
+# Learn more here
+# https://www.stat.auckland.ac.nz/~paul/useR2015-grid/grid-slides.html
+# https://bookdown.org/rdpeng/RProgDA/the-grid-package.html#grobs
+
+shift_xaxis_facet <- function(p, y=0){
+  g <- ggplotGrob(p)
+  dummy <- data.frame(y=y)
+  # Identify axis elements - use this to find and move other elements
+  ax <- g[["grobs"]][g$layout$name == "axis-b-1-1"][[1]]
+  # plot grid as an annotation
+  # grobTree combines all the elements of the axis into a single grob (vp = vertical position)
+  # Viewport defines a rectangular window  
+  p + annotation_custom(grid::grobTree(ax, vp = grid::viewport(y=1, height=sum(ax$height))), 
+                        ymax=y, ymin=y) +
+    geom_hline(aes(yintercept=y), data = dummy) +
+    theme(axis.text.x = element_blank(), 
+          axis.ticks.x = element_blank())
+  
+}
+
+shift_xaxis <- function(p, y=0){
+  g <- ggplotGrob(p)
+  dummy <- data.frame(y=y)
+  # Identify axis elements - use this to find and move other elements
+  ax <- g[["grobs"]][g$layout$name == "axis-b"][[1]]
+  # plot grid as an annotation
+  # grobTree combines all the elements of the axis into a single grob (vp = vertical position)
+  # Viewport defines a rectangular window  
+  p + annotation_custom(grid::grobTree(ax, vp = grid::viewport(y=1, height=sum(ax$height))), 
+                        ymax=y, ymin=y) +
+    geom_hline(aes(yintercept=y), data = dummy) +
+    theme(axis.text.x = element_blank(), 
+          axis.ticks.x = element_blank(),
+          axis.line.x = element_blank())
+  
+}
+
+
+
+# Load Data ---------------------------------------------------------------
+
+folderpath <- here("rawdata","Marios","1_SpecificCI","CombinedData")
+filename <- "CI_ProcessedData_pertrial_1sbins.csv"
+
+rawdata <- read_csv(here(folderpath,filename))
+
+data_averagepersession <- rawdata %>% 
+  group_by(Day, subject, CS_name, Period) %>% 
+  summarise(MagEntries = mean(A3_freq)*10,
+            MagDuration = mean(A3_dur)*10) %>%
+  ungroup()
+
+
+Acqsuisition_Stage1_MagFreq <- data_averagepersession %>% 
+  filter(Period == "CS") %>%
+  ggplot(mapping = aes(x = as.factor(Day), y = MagEntries, group = CS_name, colour = CS_name, fill = CS_name, shape = CS_name,linetype = CS_name)) +
+  stat_summary_bin(fun.data = "mean_se", geom = "line", size = .5) +
+  stat_summary(fun.data = "mean_se", geom = "errorbar", width = 0.0, size = .3, linetype = 1, show.legend = FALSE) +
+  stat_summary_bin(fun.data = "mean_se", geom = "point", size = 2) +
+  # Make Pretty
+  scale_y_continuous( expand = expansion(mult = c(0, 0)), breaks=seq(-100,100,1)) +
+  ggtitle("Acquisition") + xlab("Day") + ylab("Magazine Entry (CS)") +
+  theme_cowplot(11) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(plot.title = element_text(size=10)) +
+  coord_cartesian(ylim = c(0,6.0001)) +
+  theme(axis.title.x=element_text(face = "bold")) 
+  # scale_linetype_manual(name = "", values = linetypes)  +
+  # scale_colour_manual(name = "", values = linecolours, aesthetics = c("colour")) +
+  # scale_shape_manual(name = "", values = pointshapes) +
+  # scale_fill_manual(name = "", values = fillcolours) +
+  # theme(legend.key.width=unit(1,"line"))
+
